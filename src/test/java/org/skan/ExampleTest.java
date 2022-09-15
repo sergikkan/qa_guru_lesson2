acc7e8616580d6bd0b74c9792a531458cd28082a
kans <sergik-199022@mail.ru>
2022-09-15 16:48:45 +0600

    use random

--- src/test/java/org/skan/ExampleTest.java
+++ src/test/java/org/skan/ExampleTest.java
@@ -3,15 +3,17 @@ package org.skan;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
 
     @Test
     void exampleTest1(){
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(new Random().nextInt() >1);
     }
 
     @Test
     void exampleTest2(){
-        Assertions.assertTrue(5>3);
+        Assertions.assertTrue(new Random().nextInt()>3);
     }
 }
